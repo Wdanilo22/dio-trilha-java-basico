01ee76ff1e9a8f92b5fef361d5faea0d5e7953e2
Wdanilo22 <106202536+Wdanilo22@users.noreply.github.com>
2024-06-07 15:57:31 -0300

    ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        int Numero;
+        String Agencia;
+        String NomeCliente;
+        double Saldo;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Informe o número da conta: ");
+        Numero = input.nextInt();
+        input.nextLine();  
+
+        System.out.println("Informe a Agência: ");
+        Agencia = input.nextLine();
+
+        System.out.println("Informe seu nome: ");
+        NomeCliente = input.nextLine();
+
+        System.out.println("Informe o saldo da conta: ");
+        Saldo = input.nextDouble();
+
+        input.close();
+
+        System.out.println("Número da Conta: " + Numero);
+        System.out.println("Agência: " + Agencia);
+        System.out.println("Nome do Cliente: " + NomeCliente);
+        System.out.println("Saldo: " + Saldo);
+
     }
 }
